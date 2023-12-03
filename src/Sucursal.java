8ecabb5242f77faa54328f0f4c90b7669b2b25fe
juanfabre <136760587+juanfabre@users.noreply.github.com>
2023-12-03 19:37:14 -0300

    Update Sucursal.java

--- src/Sucursal.java
+++ src/Sucursal.java
@@ -3,21 +3,22 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Sucursal {
+    public String id;
     public String nombre;
     public String ciudad;
-    public List<Empleado> empleadosSucursal;
-
-    public Sucursal(String pnombre, String pciudad) {
+    public Sucursal(String pid,String pnombre, String pciudad) {
+        this.id = pid;
         this.nombre = pnombre;
         this.ciudad = pciudad;
-        this.empleadosSucursal = new ArrayList<>();
+
     }
 
-    public void agregarEmpleado(Empleado empleado) {
-        empleadosSucursal.add(empleado);
+    public String getId() {
+        return id;
     }
-    public List<Empleado> obtenerEmpleados() {
-        return empleadosSucursal;
+
+    public void setId(String id) {
+        this.id = id;
     }
 
     public String getNombre() {
@@ -28,18 +29,65 @@ public class Sucursal {
         this.nombre = nombre;
     }
 
-    public List<Empleado> getEmpleadosSucursal() {
-        return empleadosSucursal;
+    public String getCiudad() {
+        return ciudad;
+    }
+    public void setCiudad(String ciudad) {
+        this.ciudad = ciudad;
     }
 
-    public void setEmpleadosSucursal(List<Empleado> empleadosSucursal) {
-        this.empleadosSucursal = empleadosSucursal;
+    @Override
+    public String toString() {
+        return "Sucursal{" +
+                "id=" + id +
+                ", nombre='" + nombre + '\'' +
+                ", ciudad=" + ciudad +
+                '}';
     }
+
+    public static void gestionSucursal(Scanner keyboard){
+        limpiarConsola();
+        boolean salir = false;
+        while(salir == false){
+            System.out.println("Ingrese la opcion deseada\n" +
+                    "1- Alta Sucursal \n" +
+                    "2- Actualizar Sucursal \n" +
+                    "3- Eliminar Sucursal \n" +
+                    "4- Listar Sucursal \n" +
+                    "0- Volver");
+            //     try {
+            short opcion4 = keyboard.nextShort();
+            switch (opcion4) {
+                case 1:
+                    altaSucursal(keyboard);
+                    break;
+                case 2:
+                    actualizarSucursal(keyboard);
+                    break;
+                case 3:
+                    eliminarSucursal(keyboard);
+                    break;
+                case 4:
+                    listarSucursal();
+                    break;
+                case 0:
+                    salir = true;
+                    break;
+                default:
+                    System.out.println("La opcion ingresada no es correcta");
+                    break;
+            }
+        }
+    }
+
 
     static ArrayList<Sucursal> pListaSucursal = new ArrayList<Sucursal>();
     public static void altaSucursal(Scanner keyboard){
         Scanner scanner = new Scanner(System.in);
         System.out.println("Ingrese los datos de la sucursal:");
+
+        System.out.print("id: ");
+        String pid = scanner.nextLine();
 
         System.out.print("Nombre: ");
         String pNombre = scanner.nextLine();
@@ -47,10 +95,7 @@ public class Sucursal {
         System.out.print("ciudad: ");
         String  pciudad = scanner.nextLine();
 
-        System.out.print("ciudad: ");
-
-
-        Sucursal pSucursal = new Sucursal(pNombre,pciudad);
+        Sucursal pSucursal = new Sucursal(pid,pNombre,pciudad);
         long cantidad = pListaSucursal.size();
         pListaSucursal.add(pSucursal);
         if(pListaSucursal.size() > cantidad){
@@ -59,6 +104,80 @@ public class Sucursal {
         else{
             System.out.println("Error al guardar una Sucursal");
         }
+    }
+
+    public static void eliminarSucursal(Scanner keyboard){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Ingrese el nombre la sucursal que quiere eliminar:");
+        String Enombre = scanner.nextLine();
+
+        Sucursal sucursalE = null;
+        for (Sucursal sucursal : pListaSucursal) {
+            if (sucursal.getNombre().equals(Enombre)) {
+                sucursalE = sucursal;
+                pListaSucursal.remove(sucursalE);
+                System.out.println("la sucursal se eliminó correctamente.");
+                listarSucursal();
+                break;
+            }
+        }
+    }
+    public static void listarSucursal(){
+        System.out.println("Lista de sucursal:");
+        for (Sucursal sucursal : pListaSucursal) {
+            System.out.println(sucursal);
+        }
+    }
+    public static void actualizarSucursal(Scanner keyboard){
+        keyboard.nextLine();
+        listarSucursal();
+        System.out.println("Ingresa la id de la sucursal que deseas modificar:");
+        String pid = keyboard.nextLine();
+        Sucursal pSucursal = buscarSucursal(pid);
+        boolean salir = false;
+        while(salir == false) {
+            if (pSucursal != null) {
+                System.out.println("Seleccione lo que desea modificar: \n" +
+                        "1 - Nombre: \n" +
+                        "2 - Ciudad: \n" +
+                        "0 - Volver");
+                int opcion = keyboard.nextInt();
+                keyboard.nextLine();
+                switch (opcion) {
+                    case 1:
+                        System.out.println("Ingrese el nuevo nombre:");
+                        String nuevoNombre = keyboard.nextLine();
+                        pSucursal.setNombre(nuevoNombre);
+                        System.out.println("Nombre modificado correctamente");
+                        break;
+                    case 2:
+                        System.out.println("Ingrese la nueva Ciudad:");
+                        String nuevaCiudad = keyboard.nextLine();
+                        pSucursal.setCiudad(nuevaCiudad);
+                        System.out.println("Ciudad modificado correctamente");
+                        break;
+                    case 0:
+                        salir = true;
+                        break;
+                    default:
+                        System.out.println("Opción Incorrecta");
+                        break;
+                }
+            }
+            else{
+                System.out.println("El empleado no existe");
+            }
+        }
+    }
+    public static Sucursal buscarSucursal(String pid){
+        Sucursal lapSucursal = null;
+        for (Sucursal pSucursal : pListaSucursal){
+            if(pSucursal.getId().equalsIgnoreCase(pid)){
+                lapSucursal = pSucursal;
+                break;
+            }
+        }
+        return lapSucursal;
     }
 
 
