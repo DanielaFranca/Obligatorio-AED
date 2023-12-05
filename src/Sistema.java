60bcf566880a00d3b2c645560434b3f28f56df13
juanfabre <136760587+juanfabre@users.noreply.github.com>
2023-12-05 20:20:47 -0300

    Update Sistema.java

--- src/Sistema.java
+++ src/Sistema.java
@@ -7,11 +7,14 @@ public class Sistema {
         boolean salir = false;
         while(salir == false) {
 
-            System.out.println("Bienvenido!\n" +
+            System.out.println(" !\n" +
                     "Ingrese la opcion deseada\n" +
                     "1- Gestionar Empleados \n" +
                     "2- Gestionar Empresas \n" +
                     "3- Gestionar Sucursales \n" +
+                    "4- Listar los empleados dada una sección ordenados de mayor a menor jerarquía \n" +
+                    "5- Listar los empleados de una sucursal ordenados de mayor a menor jerarquía \n" +
+                    "6- Listar los datos de un empleado dado por su identificador y de todos sus subordinados \n" +
                     "0- Salir");
             try {
                 short opcion = keyboard.nextShort();
@@ -21,12 +24,24 @@ public class Sistema {
                         pEmpleado.gestionEmpleados(keyboard);
                         break;
                     case 2:
-                    //    MetodosTecnico pMetodoT = new MetodosTecnico();
-                    //    pMetodoT.gestionTecnicos(keyboard);
+                        Empresa pEmpresa = new Empresa();
+                        pEmpresa.gestionEmpresas(keyboard);
                         break;
                     case 3:
-                    //    MetodosEquipo pMetodoE = new MetodosEquipo();
-                    //    pMetodoE.gestionarEquipos(keyboard);
+                        Sucursal pSucursal = new Sucursal();
+                        pSucursal.gestionSucursal(keyboard);
+                        break;
+                    case 4:
+                        ArbolABB pArbolABB = new ArbolABB();
+                        pArbolABB.preOrderSeccion(keyboard);
+                        break;
+                    case 5:
+                        ArbolABB aArbolABB = new ArbolABB();
+                        aArbolABB.preOrderSucursal(keyboard);
+                        break;
+                    case 6:
+                        ArbolABB bArbolABB = new ArbolABB();
+                        bArbolABB.preOrderSubordinados(keyboard);
                         break;
                     case 0:
                         salir = true;
